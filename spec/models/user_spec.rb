9384ae23847436ab08966b6bc730a16c29fc5ab3
Andrey Rabotinsky <robotgo@inbox.ru>
2014-06-27 11:44:31 +0400

    Excercises after part 6

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,16 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should be_valid }
 
+  describe "email address with mixed case" do
+  	let(:mixed_case_email) { "Foo@ExaMpLe.cOm" }
+
+	it "should be saved ass all lower-case" do
+		@user.email = mixed_case_email
+		@user.save
+		expect(@user.reload.email).to eq mixed_case_email.downcase
+	end
+  end
+
   describe "with a password that's too short" do
   	before { @user.password = @user.password_confirmation = "a" * 5 }
   	it { should be_invalid }
@@ -75,7 +85,7 @@ describe User do
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
+                     foo@bar_baz.com foo@bar+baz.com foo@bat..com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
