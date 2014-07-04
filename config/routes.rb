d26ba29285aa8ef070535abb1a89813f85cf5608
Andrey Rabotinsky <robotgo@inbox.ru>
2014-07-04 13:30:53 +0400

    Add login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 SampleApp::Application.routes.draw do
   
+  resources :sessions, only: [:new, :create, :destroy]  
   resources :users
   root 	'static_pages#home'
   match '/contact',	to: 'static_pages#contact',	via: 'get'
   match '/help',    	to: 'static_pages#help',	via: 'get'
   match '/about',	to: 'static_pages#about',	via: 'get'
   match '/signup',	to: 'users#new',		via: 'get'
+  match '/signin',	to: 'sessions#new',		via: 'get'
+  match '/signout',	to: 'sessions#destroy',		via: 'delete' 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
