8d1d88e7c33bdd9ec438893e2732abf8805a06b3
Andrey Rabotinsky <robotgo@inbox.ru>
2014-07-04 13:52:14 +0400

    Finish sign in

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -13,18 +13,14 @@ describe "Authentication" do
 		describe "with invalid informtaion" do
 			before { click_button signin}
 
-			it { should have_selector('div.alert.alert-error') }
+			it { should have_error_message('Invalid') }
 			it { should have_title('Sign in') }
                 	it { should have_content('Sign in') }
 		end
 		
 		describe "with valid information" do
 			let(:user) { FactoryGirl.create(:user) }
-			before do
-				fill_in "Email",    with: user.email.upcase
-    				fill_in "Password", with: user.password
-				click_button signin
-			end
+			before { valid_signin(user) }
 
 			it { should have_title(user.name) }
 			it { should have_link('Profile',	href: user_path(user)) }
@@ -38,7 +34,7 @@ describe "Authentication" do
 
 			describe "after visiting another page" do
 				before { click_link "Home" }
-				it { should_not have_selector('div.alert.alert-error') }
+				it { should_not have_error_message('Invalid') }
 			end
 
 		end
